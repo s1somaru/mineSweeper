0dcb9800382f0495c1c62f5e454662951462519d
川本璃玖斗 <sisomaru1207@gmail.com>
2025-12-23 15:55:30 +0900

    minesweeper.pyを分割した．
    consts.py:設定をまとめたファイル
    logic.py:描画機能を一切使わない純粋な計算部分をまとめたファイル．
    view.py:描画に関する処理をクラス（MinesweeperView）にまとめた．
    main.py:実行用ファイル `python main.py` で実行できる．

---
+++ consts.py
@@ -0,0 +1,18 @@
+# consts.py
+# --- 設定 ---
+CELL_SIZE = 30 
+
+# --- 状態定数 ---
+MINE = -1
+UNOPENED = -2
+FLAGGED = -3 
+
+# --- 色定義 ---
+COLOR_UNOPENED = "lightgray"
+COLOR_MINE = "red"
+COLOR_OPENED = "white"
+COLOR_TEXT = "blue"
+COLOR_FLAG = "orange"
+COLOR_BTN_OPEN = "lightblue"
+COLOR_BTN_FLAG = "orange"
+COLOR_BG = "gray"
